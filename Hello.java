f8e5c7fa6c41a5fa227438307f2c249fd9d2a262
Irshad <may.irshad@gmail.com>
2023-02-28 13:16:37 -0800

    init

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("303093687 - This line was added by Ahmad Irshad Mayaar.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
